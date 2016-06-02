20edd82b30c22b7d7b4e0c2a6ed4e26e64e9d43f
U999347 <asterisk.loftis@vantiv.com>
2016-06-02 10:33:26 -0600

    Added homepage imagery remember to change gulpfile.js settings

--- foundation/gulpfile.js
+++ foundation/gulpfile.js
@@ -16,12 +16,12 @@ gulp.task('sass', function() {
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     // For windows path
-    // .pipe(gulp.dest('C:/MAMP/htdocs/vantiv/sites/all/themes/clean7/foundation_clean7/css')
-    //     .on('error', $.sass.logError))
+     .pipe(gulp.dest('C:/MAMP/htdocs/vantiv/sites/all/themes/clean7/foundation_clean7/css')
+         .on('error', $.sass.logError))
 
     // For Mac path
-    .pipe(gulp.dest('/Applications/MAMP/htdocs/vantiv/sites/all/themes/clean7/foundation_clean7/css')
-      .on('error', $.sass.logError))
+   // .pipe(gulp.dest('/Applications/MAMP/htdocs/vantiv/sites/all/themes/clean7/foundation_clean7/css')
+   //   .on('error', $.sass.logError))
 
     // For foundation project
     .pipe(gulp.dest('css')
